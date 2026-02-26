7fa2d2f5befe6be57c634bfd0546f42d515279d6
iFryno <thealmightyfryno@gmail.com>
2026-02-26 21:49:14 +0100

    Update Black Theme.ps1

--- Black Theme.ps1
+++ Black Theme.ps1
@@ -11,6 +11,7 @@
     Write-Host ""
     Write-Host "1. Theme: Black"
     Write-Host "2. Theme: Default"
+    Write-Host ""
     while ($true) {
     $choice = Read-Host " "
     if ($choice -match '^[1-2]$') {
