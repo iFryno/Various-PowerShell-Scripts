581a8b84c6d8e12eee07f375ffa660ff86bdeb74
iFryno <thealmightyfryno@gmail.com>
2026-03-06 02:01:37 +0100

    Update Legacy File Explorer.ps1

--- Legacy File Explorer.ps1
+++ Legacy File Explorer.ps1
@@ -23,7 +23,7 @@ Clear-Host
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
 
-; w10 file explorer ribbon
+; legacy file explorer
 [HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}]
 @="CLSID_ItemsViewAdapter"
 
@@ -55,15 +55,15 @@ Windows Registry Editor Version 5.00
 "MinimizedStateTabletModeOn"=dword:00000001
 "MinimizedStateTabletModeOff"=dword:00000001
 "@
-Set-Content -Path "$env:TEMP\W10FileExplorer.reg" -Value $MultilineComment -Force
+Set-Content -Path "$env:TEMP\LegacyExplorer.reg" -Value $MultilineComment -Force
 # edit reg file
-$path = "$env:TEMP\W10FileExplorer.reg"
+$path = "$env:TEMP\LegacyExplorer.reg"
 (Get-Content $path) -replace "\?","$" | Out-File $path
 # import reg file
-Regedit.exe /S "$env:TEMP\W10FileExplorer.reg"
-# restart explorer
+Regedit.exe /S "$env:TEMP\LegacyExplorer.reg"
+# stop explorer
 Stop-Process -Force -Name explorer -ErrorAction SilentlyContinue | Out-Null
-# open file explorer
+# start explorer
 Start-Process explorer.exe
 exit
 
@@ -75,7 +75,7 @@ Clear-Host
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
 
-; w11 file explorer ribbon
+; default file explorer
 [-HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}]
 [-HKEY_CURRENT_USER\Software\Classes\CLSID\{6480100b-5a83-4d1e-9f69-8ae5a88e9a33}]
 
@@ -86,15 +86,15 @@ Windows Registry Editor Version 5.00
 "MinimizedStateTabletModeOn"=-
 "MinimizedStateTabletModeOff"=-
 "@
-Set-Content -Path "$env:TEMP\W11FileExplorer.reg" -Value $MultilineComment -Force
+Set-Content -Path "$env:TEMP\DefaultExplorer.reg" -Value $MultilineComment -Force
 # edit reg file
-$path = "$env:TEMP\W11FileExplorer.reg"
+$path = "$env:TEMP\DefaultExplorer.reg"
 (Get-Content $path) -replace "\?","$" | Out-File $path
 # import reg file
-Regedit.exe /S "$env:TEMP\W11FileExplorer.reg"
-# restart explorer
+Regedit.exe /S "$env:TEMP\DefaultExplorer.reg"
+# stop explorer
 Stop-Process -Force -Name explorer -ErrorAction SilentlyContinue | Out-Null
-# open file explorer
+# start explorer
 Start-Process explorer.exe
 exit
 
