cb0e42a726754745cff199f9a18c6a1e2c2e3c12
iFryno <thealmightyfryno@gmail.com>
2026-04-06 23:47:19 +0200

    Update Legacy File Explorer.ps1

--- Legacy File Explorer.ps1
+++ Legacy File Explorer.ps1
@@ -1,12 +1,16 @@
-    If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
-    {Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-    Exit}
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
-    $Host.UI.RawUI.BackgroundColor = "Black"
-	$Host.PrivateData.ProgressBackgroundColor = "Black"
-    $Host.PrivateData.ProgressForegroundColor = "White"
-    Clear-Host
+	# ADMINISTRATOR PRIVILEGES
+	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+	Exit}
 
+	# WINDOW SETTINGS
+	$Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
+	$Host.UI.RawUI.BackgroundColor = "Black"
+	$Host.PrivateData.ProgressBackgroundColor = "Black"
+	$Host.PrivateData.ProgressForegroundColor = "White"
+	Clear-Host
+
+	# INPUT UI
     Write-Host "Restore the Legacy File Explorer on Windows 11`n"
     Write-Host "1. File Explorer: Legacy"
     Write-Host "2. File Explorer: Default`n"
@@ -17,11 +21,12 @@
     1 {
 
 Clear-Host
-# create reg file
+
+# CREATE REG FILE
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
 
-; legacy file explorer
+; LEGACY FILE EXPLORER
 [HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}]
 @="CLSID_ItemsViewAdapter"
 
@@ -53,27 +58,34 @@ Windows Registry Editor Version 5.00
 "MinimizedStateTabletModeOff"=dword:00000000
 "MinimizedStateTabletModeOn"=dword:00000001
 "@
-# save reg file
+
+# SAVE REG FILE
 Set-Content -Path "$env:SystemRoot\Temp\LegacyExplorer.reg" -Value $MultilineComment -Force
-# import reg file
+
+# IMPORT REG FILE
 Start-Process -Wait "regedit.exe" -ArgumentList "/S `"$env:SystemRoot\Temp\LegacyExplorer.reg`"" -WindowStyle Hidden
-# delete reg file
+
+# DELETE REG FILE
 Remove-Item "$env:SystemRoot\Temp\LegacyExplorer.reg" -Force
-# stop explorer
+
+# STOP EXPLORER
 Stop-Process -Force -Name explorer -ErrorAction SilentlyContinue | Out-Null
-# start explorer
+
+# START EXPLORER
 Start-Process explorer.exe
+
 exit
 
       }
     2 {
 
 Clear-Host
-# create reg file
+
+# CREATE REG FILE
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
 
-; default file explorer
+; DEFAULT FILE EXPLORER
 [-HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}]
 [-HKEY_CURRENT_USER\Software\Classes\CLSID\{6480100b-5a83-4d1e-9f69-8ae5a88e9a33}]
 
@@ -84,17 +96,23 @@ Windows Registry Editor Version 5.00
 "MinimizedStateTabletModeOff"=-
 "MinimizedStateTabletModeOn"=-
 "@
-# save reg file
+
+# SAVE REG FILE
 Set-Content -Path "$env:SystemRoot\Temp\DefaultExplorer.reg" -Value $MultilineComment -Force
-# import reg file
+
+# IMPORT REG FILE
 Start-Process -Wait "regedit.exe" -ArgumentList "/S `"$env:SystemRoot\Temp\DefaultExplorer.reg`"" -WindowStyle Hidden
-# delete reg file
+
+# DELETE REG FILE
 Remove-Item "$env:SystemRoot\Temp\DefaultExplorer.reg" -Force
-# stop explorer
+
+# STOP EXPLORER
 Stop-Process -Force -Name explorer -ErrorAction SilentlyContinue | Out-Null
-# start explorer
+
+# START EXPLORER
 Start-Process explorer.exe
+
 exit
 
       }
-    } } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" } }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
