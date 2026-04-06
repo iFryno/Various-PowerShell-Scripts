c3c4b7490af10c9ddae33fb6e58e8346c2ff13fd
iFryno <thealmightyfryno@gmail.com>
2026-04-06 23:46:39 +0200

    Update Digital Markets Act.ps1

--- Digital Markets Act.ps1
+++ Digital Markets Act.ps1
@@ -1,15 +1,19 @@
-    If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
-    {Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-    Exit}
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
-    $Host.UI.RawUI.BackgroundColor = "Black"
-	$Host.PrivateData.ProgressBackgroundColor = "Black"
-    $Host.PrivateData.ProgressForegroundColor = "White"
-    Clear-Host
+	# ADMINISTRATOR PRIVILEGES
+	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+	Exit}
 
+	# WINDOW SETTINGS
+	$Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
+	$Host.UI.RawUI.BackgroundColor = "Black"
+	$Host.PrivateData.ProgressBackgroundColor = "Black"
+	$Host.PrivateData.ProgressForegroundColor = "White"
+	Clear-Host
+
+	# INPUT UI
     Write-Host "EU Digital Markets Act (DMA)`n"
 	Write-Host "1. DMA: Enabled"
-	Write-Host "2. DMA: Disabled (Default)`n"
+	Write-Host "2. DMA: Default`n"
     while ($true) {
     $choice = Read-Host " "
     if ($choice -match '^[1-2]$') {
@@ -17,23 +21,41 @@
     1 {
 
 Clear-Host
+
+# CREATE REG1.EXE
 Copy-Item (Get-Command reg.exe).Source .\reg1.exe -Force -EA 0
+
+# SET DEVICE SETUP REGION TO IRELAND
 & .\reg1.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Control Panel\DeviceRegion" /v DeviceRegion /t REG_DWORD /d 68 /f *>$null
+
+# REMOVE REG1.EXE
 Remove-Item .\reg1.exe -Force -EA 0
+
 Write-Host "Restart to apply..."
+
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
 exit
 
       }
     2 {
 
 Clear-Host
+
+# CREATE REG1.EXE
 Copy-Item (Get-Command reg.exe).Source .\reg1.exe -Force -EA 0
+
+# SET DEVICE SETUP REGION TO UNITED STATES
 & .\reg1.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Control Panel\DeviceRegion" /v DeviceRegion /t REG_DWORD /d 244 /f *>$null
+
+# REMOVE REG1.EXE
 Remove-Item .\reg1.exe -Force -EA 0
+
 Write-Host "Restart to apply..."
+
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
 exit
 
       }
-    } } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" } }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
