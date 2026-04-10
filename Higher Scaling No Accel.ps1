2a1b0fbe35244b981f618ee3411666376f4d678e
iFryno <thealmightyfryno@gmail.com>
2026-04-10 06:12:34 +0200

    Update Higher Scaling No Accel.ps1

--- Higher Scaling No Accel.ps1
+++ Higher Scaling No Accel.ps1
@@ -1,5 +1,5 @@
 	# ADMINISTRATOR PRIVILEGES
-	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
 	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
 	Exit}
 
@@ -17,10 +17,11 @@
     Write-Host "3. 150%"
     Write-Host "4. 175%"
     Write-Host "5. 200%"
-    Write-Host "6. 225%`n"
+    Write-Host "6. 225%"
+	Write-Host "7. Default`n"
     while ($true) {
     $choice = Read-Host " "
-    if ($choice -match '^[1-6]$') {
+    if ($choice -match '^[1-7]$') {
     switch ($choice) {
     1 {
 
@@ -42,25 +43,25 @@ Windows Registry Editor Version 5.00
 "MouseThreshold1"="0"
 "MouseThreshold2"="0"
 
-; 100% SCALING CURVE
+; 100% NO SCALING CURVE
 [HKEY_CURRENT_USER\Control Panel\Mouse]
 "SmoothMouseXCurve"=hex:\
-    00,00,00,00,00,00,00,00,\
-	c0,cc,0c,00,00,00,00,00,\
-	80,99,19,00,00,00,00,00,\
-	40,66,26,00,00,00,00,00,\
-	00,33,33,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+c0,cc,0c,00,00,00,00,00,\
+80,99,19,00,00,00,00,00,\
+40,66,26,00,00,00,00,00,\
+00,33,33,00,00,00,00,00
 "SmoothMouseYCurve"=hex:\
-    00,00,00,00,00,00,00,00,\
-	00,00,38,00,00,00,00,00,\
-	00,00,70,00,00,00,00,00,\
-	00,00,a8,00,00,00,00,00,\
-	00,00,e0,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,38,00,00,00,00,00,\
+00,00,70,00,00,00,00,00,\
+00,00,a8,00,00,00,00,00,\
+00,00,e0,00,00,00,00,00
 
 ; 100% DPI SCALING
 [HKEY_CURRENT_USER\Control Panel\Desktop]
-"Win8DpiScaling"=dword:00000001
 "LogPixels"=dword:00000060
+"Win8DpiScaling"=dword:00000001
 
 ; DISABLE FIX SCALING FOR APPS
 [HKEY_CURRENT_USER\Control Panel\Desktop]
@@ -105,25 +106,25 @@ Windows Registry Editor Version 5.00
 "MouseThreshold1"="6"
 "MouseThreshold2"="10"
 
-; 125% SCALING CURVE
+; 125% NO SCALING CURVE
 [HKEY_CURRENT_USER\Control Panel\Mouse]
 "SmoothMouseXCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	00,00,10,00,00,00,00,00,\
-	00,00,20,00,00,00,00,00,\
-	00,00,30,00,00,00,00,00,\
-	00,00,40,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,10,00,00,00,00,00,\
+00,00,20,00,00,00,00,00,\
+00,00,30,00,00,00,00,00,\
+00,00,40,00,00,00,00,00
 "SmoothMouseYCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	00,00,38,00,00,00,00,00,\
-	00,00,70,00,00,00,00,00,\
-	00,00,A8,00,00,00,00,00,\
-	00,00,E0,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,38,00,00,00,00,00,\
+00,00,70,00,00,00,00,00,\
+00,00,A8,00,00,00,00,00,\
+00,00,E0,00,00,00,00,00
 
 ; 125% DPI SCALING
 [HKEY_CURRENT_USER\Control Panel\Desktop]
-"Win8DpiScaling"=dword:00000001
 "LogPixels"=dword:00000078
+"Win8DpiScaling"=dword:00000001
 
 ; ENABLE FIX SCALING FOR APPS
 [HKEY_CURRENT_USER\Control Panel\Desktop]
@@ -168,25 +169,25 @@ Windows Registry Editor Version 5.00
 "MouseThreshold1"="6"
 "MouseThreshold2"="10"
 
-; 150% SCALING CURVE
+; 150% NO SCALING CURVE
 [HKEY_CURRENT_USER\Control Panel\Mouse]
 "SmoothMouseXCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	30,33,13,00,00,00,00,00,\
-	60,66,26,00,00,00,00,00,\
-	90,99,39,00,00,00,00,00,\
-	C0,CC,4C,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+30,33,13,00,00,00,00,00,\
+60,66,26,00,00,00,00,00,\
+90,99,39,00,00,00,00,00,\
+C0,CC,4C,00,00,00,00,00
 "SmoothMouseYCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	00,00,38,00,00,00,00,00,\
-	00,00,70,00,00,00,00,00,\
-	00,00,A8,00,00,00,00,00,\
-	00,00,E0,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,38,00,00,00,00,00,\
+00,00,70,00,00,00,00,00,\
+00,00,A8,00,00,00,00,00,\
+00,00,E0,00,00,00,00,00
 
 ; 150% DPI SCALING
 [HKEY_CURRENT_USER\Control Panel\Desktop]
-"Win8DpiScaling"=dword:00000001
 "LogPixels"=dword:00000090
+"Win8DpiScaling"=dword:00000001
 
 ; ENABLE FIX SCALING FOR APPS
 [HKEY_CURRENT_USER\Control Panel\Desktop]
@@ -231,25 +232,25 @@ Windows Registry Editor Version 5.00
 "MouseThreshold1"="6"
 "MouseThreshold2"="10"
 
-; 175% SCALING CURVE
+; 175% NO SCALING CURVE
 [HKEY_CURRENT_USER\Control Panel\Mouse]
 "SmoothMouseXCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	60,66,16,00,00,00,00,00,\
-	C0,CC,2C,00,00,00,00,00,\
-	20,33,43,00,00,00,00,00,\
-	80,99,59,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+60,66,16,00,00,00,00,00,\
+C0,CC,2C,00,00,00,00,00,\
+20,33,43,00,00,00,00,00,\
+80,99,59,00,00,00,00,00
 "SmoothMouseYCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	00,00,38,00,00,00,00,00,\
-	00,00,70,00,00,00,00,00,\
-	00,00,A8,00,00,00,00,00,\
-	00,00,E0,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,38,00,00,00,00,00,\
+00,00,70,00,00,00,00,00,\
+00,00,A8,00,00,00,00,00,\
+00,00,E0,00,00,00,00,00
 
 ; 175% DPI SCALING
 [HKEY_CURRENT_USER\Control Panel\Desktop]
-"Win8DpiScaling"=dword:00000001
 "LogPixels"=dword:000000a8
+"Win8DpiScaling"=dword:00000001
 
 ; ENABLE FIX SCALING FOR APPS
 [HKEY_CURRENT_USER\Control Panel\Desktop]
@@ -294,25 +295,25 @@ Windows Registry Editor Version 5.00
 "MouseThreshold1"="6"
 "MouseThreshold2"="10"
 
-; 200% SCALING CURVE
+; 200% NO SCALING CURVE
 [HKEY_CURRENT_USER\Control Panel\Mouse]
 "SmoothMouseXCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	90,99,19,00,00,00,00,00,\
-	20,33,33,00,00,00,00,00,\
-	B0,CC,4C,00,00,00,00,00,\
-	40,66,66,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+90,99,19,00,00,00,00,00,\
+20,33,33,00,00,00,00,00,\
+B0,CC,4C,00,00,00,00,00,\
+40,66,66,00,00,00,00,00
 "SmoothMouseYCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	00,00,38,00,00,00,00,00,\
-	00,00,70,00,00,00,00,00,\
-	00,00,A8,00,00,00,00,00,\
-	00,00,E0,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,38,00,00,00,00,00,\
+00,00,70,00,00,00,00,00,\
+00,00,A8,00,00,00,00,00,\
+00,00,E0,00,00,00,00,00
 
 ; 200% DPI SCALING
 [HKEY_CURRENT_USER\Control Panel\Desktop]
-"Win8DpiScaling"=dword:00000001
 "LogPixels"=dword:000000c0
+"Win8DpiScaling"=dword:00000001
 
 ; ENABLE FIX SCALING FOR APPS
 [HKEY_CURRENT_USER\Control Panel\Desktop]
@@ -357,25 +358,25 @@ Windows Registry Editor Version 5.00
 "MouseThreshold1"="6"
 "MouseThreshold2"="10"
 
-; 225% SCALING CURVE
+; 225% NO SCALING CURVE
 [HKEY_CURRENT_USER\Control Panel\Mouse]
 "SmoothMouseXCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	C0,CC,1C,00,00,00,00,00,\
-	80,99,39,00,00,00,00,00,\
-	40,66,56,00,00,00,00,00,\
-	00,33,73,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+C0,CC,1C,00,00,00,00,00,\
+80,99,39,00,00,00,00,00,\
+40,66,56,00,00,00,00,00,\
+00,33,73,00,00,00,00,00
 "SmoothMouseYCurve"=hex:\
-	00,00,00,00,00,00,00,00,\
-	00,00,38,00,00,00,00,00,\
-	00,00,70,00,00,00,00,00,\
-	00,00,A8,00,00,00,00,00,\
-	00,00,E0,00,00,00,00,00
+00,00,00,00,00,00,00,00,\
+00,00,38,00,00,00,00,00,\
+00,00,70,00,00,00,00,00,\
+00,00,A8,00,00,00,00,00,\
+00,00,E0,00,00,00,00,00
 
 ; 225% DPI SCALING
 [HKEY_CURRENT_USER\Control Panel\Desktop]
-"Win8DpiScaling"=dword:00000001
 "LogPixels"=dword:000000d8
+"Win8DpiScaling"=dword:00000001
 
 ; ENABLE FIX SCALING FOR APPS
 [HKEY_CURRENT_USER\Control Panel\Desktop]
@@ -400,4 +401,67 @@ $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 exit
 
       }
-    } } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
+    7 {
+
+Clear-Host
+
+Write-Host "Default..."
+
+# CREATE REG FILE
+$MultilineComment = @"
+Windows Registry Editor Version 5.00
+
+; DEFAULT POINTER SPEED
+[HKEY_CURRENT_USER\Control Panel\Mouse]
+"MouseSensitivity"="10"
+
+; DISABLE ENHANCE POINTER PRECISION (SHOULDNT BE ON BY DEFAULT)
+[HKEY_CURRENT_USER\Control Panel\Mouse]
+"MouseSpeed"="0"
+"MouseThreshold1"="0"
+"MouseThreshold2"="0"
+
+; DEFAULT CURVE
+[HKEY_CURRENT_USER\Control Panel\Mouse]
+"SmoothMouseXCurve"=hex:\
+00,00,00,00,00,00,00,00,\
+15,6e,00,00,00,00,00,00,\
+00,40,01,00,00,00,00,00,\
+29,dc,03,00,00,00,00,00,\
+00,00,28,00,00,00,00,00
+"SmoothMouseYCurve"=hex:\
+00,00,00,00,00,00,00,00,\
+fd,11,01,00,00,00,00,00,\
+00,24,04,00,00,00,00,00,\
+00,fc,12,00,00,00,00,00,\
+00,c0,bb,01,00,00,00,00
+
+; RESET SCALING
+[HKEY_CURRENT_USER\Control Panel\Desktop]
+"LogPixels"=-
+"Win8DpiScaling"=-
+
+; RESET FIX SCALING FOR APPS
+[HKEY_CURRENT_USER\Control Panel\Desktop]
+"EnablePerProcessSystemDPI"=-
+"@
+
+# SAVE REG FILE
+Set-Content -Path "$env:SystemRoot\Temp\Default.reg" -Value $MultilineComment -Force
+
+# IMPORT REG FILE
+Start-Process -Wait "regedit.exe" -ArgumentList "/S `"$env:SystemRoot\Temp\Default.reg`"" -WindowStyle Hidden
+
+# DELETE REG FILE
+Remove-Item "$env:SystemRoot\Temp\Default.reg" -Force
+
+Clear-Host
+
+Write-Host "Restart to apply..."
+
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+exit
+
+      }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-7).`n" -ForegroundColor Red } }
