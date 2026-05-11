3b308a6f106b00330edaca59e956d045c7fe9a15
iFryno <thealmightyfryno@gmail.com>
2026-05-11 06:37:48 +0200

    Update Start Menu Version.ps1

--- Start Menu Version.ps1
+++ Start Menu Version.ps1
@@ -1,19 +1,19 @@
-	# ADMINISTRATOR PRIVILEGES
+	# Check for administrator privileges
 	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
 	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
 	Exit}
 
-	# WINDOW SETTINGS
+	# Configure window settings
 	$Host.UI.RawUI.WindowTitle = (Split-Path -Leaf $myInvocation.MyCommand.Definition) + " (Administrator)"
 	$Host.UI.RawUI.BackgroundColor = "Black"
 	$Host.PrivateData.ProgressBackgroundColor = "Black"
 	$Host.PrivateData.ProgressForegroundColor = "White"
 	Clear-Host
 
-	# INPUT UI
+	# Show input UI
 	Write-Host "Start Menu Version`n"
-	Write-Host "1. Start Menu: 25H2"
-	Write-Host "2. Start Menu: 24H2`n"
+	Write-Host "1. 25H2"
+	Write-Host "2. 24H2`n"
 	while ($true) {
 	$choice = Read-Host " "
 	if ($choice -match '^[1-2]$') {
@@ -22,13 +22,13 @@
 
 Clear-Host
 
-# 25H2 START MENU
+# Set start menu version to 25H2
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\3036241548' /v 'EnabledState' /t REG_DWORD /d '2' /f *>$null
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\2792562829' /v 'EnabledState' /t REG_DWORD /d '2' /f *>$null
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\762256525'  /v 'EnabledState' /t REG_DWORD /d '2' /f *>$null
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\734731404'  /v 'EnabledState' /t REG_DWORD /d '2' /f *>$null
 
-# SET START MENU APPS VIEW TO LIST
+# Set start menu apps view to list
 Reg.exe add 'HKCU\Software\Microsoft\Windows\CurrentVersion\Start' /v 'AllAppsViewMode' /t REG_DWORD /d '2' /f *>$null
 
 Clear-Host
@@ -39,18 +39,18 @@ $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 exit
 
-      }
+	  }
 	2 {
 
 Clear-Host
 
-# 24H2 START MENU
+# Set start menu version to 24H2
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\3036241548' /v 'EnabledState' /t REG_DWORD /d '0' /f *>$null
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\2792562829' /v 'EnabledState' /t REG_DWORD /d '0' /f *>$null
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\762256525'  /v 'EnabledState' /t REG_DWORD /d '0' /f *>$null
 Reg.exe add 'HKLM\SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\14\734731404'  /v 'EnabledState' /t REG_DWORD /d '0' /f *>$null
 
-# RESET START MENU APPS VIEW
+# Reset start menu apps view
 Reg.exe delete 'HKCU\Software\Microsoft\Windows\CurrentVersion\Start' /v 'AllAppsViewMode' /f *>$null
 
 Clear-Host
@@ -61,5 +61,5 @@ $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 exit
 
-      }
+	  }
 	} } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
