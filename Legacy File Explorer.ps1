d5f7d478d6257b8534a13c9c70e092953e2da702
iFryno <thealmightyfryno@gmail.com>
2026-05-11 06:35:58 +0200

    Update Legacy File Explorer.ps1

--- Legacy File Explorer.ps1
+++ Legacy File Explorer.ps1
@@ -1,32 +1,32 @@
-	# ADMINISTRATOR PRIVILEGES
+	# Check for administrator privileges
 	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
 	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
 	Exit}
 
-	# WINDOW SETTINGS
+	# Configure window settings
 	$Host.UI.RawUI.WindowTitle = (Split-Path -Leaf $myInvocation.MyCommand.Definition) + " (Administrator)"
 	$Host.UI.RawUI.BackgroundColor = "Black"
 	$Host.PrivateData.ProgressBackgroundColor = "Black"
 	$Host.PrivateData.ProgressForegroundColor = "White"
 	Clear-Host
 
-	# INPUT UI
-    Write-Host "Restore the Legacy File Explorer on Windows 11`n"
-    Write-Host "1. File Explorer: Legacy"
-    Write-Host "2. File Explorer: Default`n"
-    while ($true) {
-    $choice = Read-Host " "
-    if ($choice -match '^[1-2]$') {
-    switch ($choice) {
-    1 {
+	# Show input UI
+	Write-Host "Restore the Legacy File Explorer on Windows 11`n"
+	Write-Host "1. Legacy"
+	Write-Host "2. Default`n"
+	while ($true) {
+	$choice = Read-Host " "
+	if ($choice -match '^[1-2]$') {
+	switch ($choice) {
+	1 {
 
 Clear-Host
 
-# CREATE REG FILE
+# Create reg file
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
 
-; LEGACY FILE EXPLORER
+; Restore legacy file explorer ribbon
 [HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}]
 @="CLSID_ItemsViewAdapter"
 
@@ -59,33 +59,33 @@ Windows Registry Editor Version 5.00
 "MinimizedStateTabletModeOn"=dword:00000001
 "@
 
-# SAVE REG FILE
+# Save reg file
 Set-Content -Path "$env:SystemRoot\Temp\LegacyExplorer.reg" -Value $MultilineComment -Force
 
-# IMPORT REG FILE
+# Import reg file
 Start-Process -Wait "regedit.exe" -ArgumentList "/S `"$env:SystemRoot\Temp\LegacyExplorer.reg`"" -WindowStyle Hidden
 
-# DELETE REG FILE
+# Delete reg file
 Remove-Item "$env:SystemRoot\Temp\LegacyExplorer.reg" -Force
 
-# STOP EXPLORER
+# Restart explorer
 Stop-Process -Force -Name explorer -ErrorAction SilentlyContinue | Out-Null
 
-# START EXPLORER
+# Open explorer
 Start-Process explorer.exe
 
 exit
 
-      }
-    2 {
+	  }
+	2 {
 
 Clear-Host
 
-# CREATE REG FILE
+# Create reg file
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
 
-; DEFAULT FILE EXPLORER
+; Restore default file explorer ribbon
 [-HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}]
 [-HKEY_CURRENT_USER\Software\Classes\CLSID\{6480100b-5a83-4d1e-9f69-8ae5a88e9a33}]
 
@@ -97,22 +97,22 @@ Windows Registry Editor Version 5.00
 "MinimizedStateTabletModeOn"=-
 "@
 
-# SAVE REG FILE
+# Save reg file
 Set-Content -Path "$env:SystemRoot\Temp\DefaultExplorer.reg" -Value $MultilineComment -Force
 
-# IMPORT REG FILE
+# Import reg file
 Start-Process -Wait "regedit.exe" -ArgumentList "/S `"$env:SystemRoot\Temp\DefaultExplorer.reg`"" -WindowStyle Hidden
 
-# DELETE REG FILE
+# Delete reg file
 Remove-Item "$env:SystemRoot\Temp\DefaultExplorer.reg" -Force
 
-# STOP EXPLORER
+# Restart explorer
 Stop-Process -Force -Name explorer -ErrorAction SilentlyContinue | Out-Null
 
-# START EXPLORER
+# Open explorer
 Start-Process explorer.exe
 
 exit
 
-      }
-    } } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
+	  }
+	} } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
