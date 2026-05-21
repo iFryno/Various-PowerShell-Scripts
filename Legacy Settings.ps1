feac102166afd6784bac6e23d3451c1b0e1df3e6
iFryno <thealmightyfryno@gmail.com>
2026-05-21 19:46:24 +0200

    Update Legacy Settings.ps1

--- Legacy Settings.ps1
+++ Legacy Settings.ps1
@@ -1,17 +1,14 @@
-	# Check for administrator privileges
 	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
 	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-	Exit}
+	exit}
 
-	# Configure window settings
-	$Host.UI.RawUI.WindowTitle = (Split-Path -Leaf $myInvocation.MyCommand.Definition) + " (Administrator)"
+	$Host.UI.RawUI.WindowTitle = "Administrator: " + (Split-Path -Leaf $myInvocation.MyCommand.Definition)
 	$Host.UI.RawUI.BackgroundColor = "Black"
 	$Host.PrivateData.ProgressBackgroundColor = "Black"
 	$Host.PrivateData.ProgressForegroundColor = "White"
 	Clear-Host
 
-	# Show input UI
-	Write-Host "Legacy Settings in Desktop Context Menu`n"
+	Write-Host "Legacy Settings`n"
 	Write-Host "1. Add"
 	Write-Host "2. Remove`n"
 
@@ -21,7 +18,7 @@
 	switch ($choice) {
 	1 {
 
-Clear-Host
+	Clear-Host
 
 # Add legacy settings to desktop context menu
 Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization' /v 'Icon' /t REG_SZ /d 'shell32.dll,-137' /f *>$null
@@ -38,29 +35,21 @@ Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\009flyout' /v 'I
 Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\009flyout' /v 'MUIVerb' /t REG_SZ /d 'Desktop Icon Settings' /f *>$null
 Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\009flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL desk.cpl,,0' /f *>$null
 
-Clear-Host
-
-Write-Host "Done. Press any key to exit..."
-
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
-exit
+	Write-Host "Done. Press any key to exit..."
+	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	exit
 
 	  }
 	2 {
 
-Clear-Host
+	Clear-Host
 
 # Remove legacy settings from desktop context menu
 Reg.exe delete 'HKCR\DesktopBackground\Shell\Personalization' /f *>$null
 
-Clear-Host
-
-Write-Host "Done. Press any key to exit..."
-
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
-exit
+	Write-Host "Done. Press any key to exit..."
+	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	exit
 
 	  }
-	} } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
+	} } else { Write-Host "Invalid input.`n" -ForegroundColor Red } }
