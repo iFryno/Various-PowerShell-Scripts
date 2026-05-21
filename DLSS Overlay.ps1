9f1c89f4720bab2b1ff6701c2e030528190d8e00
iFryno <thealmightyfryno@gmail.com>
2026-05-21 19:45:12 +0200

    Update DLSS Overlay.ps1

--- DLSS Overlay.ps1
+++ DLSS Overlay.ps1
@@ -1,17 +1,14 @@
-	# Check for administrator privileges
 	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
 	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-	Exit}
+	exit}
 
-	# Configure window settings
-	$Host.UI.RawUI.WindowTitle = (Split-Path -Leaf $myInvocation.MyCommand.Definition) + " (Administrator)"
+	$Host.UI.RawUI.WindowTitle = "Administrator: " + (Split-Path -Leaf $myInvocation.MyCommand.Definition)
 	$Host.UI.RawUI.BackgroundColor = "Black"
 	$Host.PrivateData.ProgressBackgroundColor = "Black"
 	$Host.PrivateData.ProgressForegroundColor = "White"
 	Clear-Host
 
-	# Show input UI
-	Write-Host "DLSS Version Overlay`n"
+	Write-Host "DLSS Overlay`n"
 	Write-Host "1. Enable"
 	Write-Host "2. Disable`n"
 
@@ -21,30 +18,24 @@
 	switch ($choice) {
 	1 {
 
-# Show DLSS overlay
+# Enable DLSS overlay
 Reg.exe add 'HKLM\SOFTWARE\NVIDIA Corporation\Global\NGXCore' /v 'ShowDlssIndicator' /t REG_DWORD /d '1024' /f *>$null
 
-Clear-Host
-
-Write-Host "Changes will take effect on next game launch..."
-
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
-exit
+	Clear-Host
+	Write-Host "Changes will take effect on next game launch..."
+	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	exit
 
 	  }
 	2 {
 
-# Remove DLSS overlay
+# Disable DLSS overlay
 Reg.exe delete 'HKLM\SOFTWARE\NVIDIA Corporation\Global\NGXCore' /v 'ShowDlssIndicator' /f *>$null
 
-Clear-Host
-
-Write-Host "Changes will take effect on next game launch..."
-
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
-exit
+	Clear-Host
+	Write-Host "Changes will take effect on next game launch..."
+	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	exit
 
 	  }
-	} } else { Write-Host "Invalid input. Please select a valid option (1-2).`n" -ForegroundColor Red } }
+	} } else { Write-Host "Invalid input.`n" -ForegroundColor Red } }
