ead43eab2b13289db0888d51691e8fe332f1899e
iFryno <thealmightyfryno@gmail.com>
2026-05-21 21:48:15 +0200

    Update Manage Windows Updates.ps1

--- Manage Windows Updates.ps1
+++ Manage Windows Updates.ps1
@@ -1,25 +1,25 @@
-	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
-	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-	exit}
+		if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+		{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+		exit}
 
-	$Host.UI.RawUI.WindowTitle = "Administrator: " + (Split-Path -Leaf $myInvocation.MyCommand.Definition)
-	$Host.UI.RawUI.BackgroundColor = "Black"
-	$Host.PrivateData.ProgressBackgroundColor = "Black"
-	$Host.PrivateData.ProgressForegroundColor = "White"
-	Clear-Host
+		$Host.UI.RawUI.WindowTitle = "Administrator: " + (Split-Path -Leaf $myInvocation.MyCommand.Definition)
+		$Host.UI.RawUI.BackgroundColor = "Black"
+		$Host.PrivateData.ProgressBackgroundColor = "Black"
+		$Host.PrivateData.ProgressForegroundColor = "White"
+		Clear-Host
 
-	Write-Host "Manage Windows Updates`n"
-	Write-Host "1. Pause"
-	Write-Host "2. Disable"
-	Write-Host "3. Enable`n"
+		Write-Host "Manage Windows Updates`n"
+		Write-Host "1. Pause"
+		Write-Host "2. Disable"
+		Write-Host "3. Enable`n"
 
-	while ($true) {
-	$choice = Read-Host " "
-	if ($choice -match '^[1-3]$') {
-	switch ($choice) {
-	1 {
+		while ($true) {
+		$choice = Read-Host " "
+		if ($choice -match '^[1-3]$') {
+		switch ($choice) {
+		1 {
 
-	Clear-Host
+		Clear-Host
 
 # Pause Windows updates for 1 year
 $pause = (Get-Date).AddDays(365)
@@ -36,13 +36,13 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Nam
 # Open Windows Update page
 Start-Process ms-settings:windowsupdate
 
-	exit
+		exit
 
-	  }
-	2 {
+		  }
+		2 {
 
-	Clear-Host
-	Write-Host "Disabling Updates. Please wait..."
+		Clear-Host
+		Write-Host "Disabling Updates. Please wait..."
 
 # Disable Windows updates
 Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' /v 'WUServer' /t REG_SZ /d 'https://blocked.invalid/' /f *>$null
@@ -60,16 +60,16 @@ Reg.exe add 'HKU\S-1-5-20\Software\Microsoft\Windows\CurrentVersion\DeliveryOpti
 # Disable Windows Update scheduled task
 Disable-ScheduledTask -TaskName 'Microsoft\Windows\WindowsUpdate\Scheduled Start' -ErrorAction SilentlyContinue | Out-Null
 
-	Clear-Host
-	Write-Host "Restart to apply..."
-	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	exit
+		Clear-Host
+		Write-Host "Restart to apply..."
+		$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+		exit
 
-	  }
-	3 {
+		  }
+		3 {
 
-	Clear-Host
-	Write-Host "Enabling Updates. Please wait..."
+		Clear-Host
+		Write-Host "Enabling Updates. Please wait..."
 
 # Enable Windows updates
 Reg.exe delete 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' /v 'WUServer' /f *>$null
@@ -87,10 +87,10 @@ Reg.exe delete 'HKU\S-1-5-20\Software\Microsoft\Windows\CurrentVersion\DeliveryO
 # Enable Windows Update scheduled task
 Enable-ScheduledTask -TaskName 'Microsoft\Windows\WindowsUpdate\Scheduled Start' -ErrorAction SilentlyContinue | Out-Null
 
-	Clear-Host
-	Write-Host "Restart to apply..."
-	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	exit
+		Clear-Host
+		Write-Host "Restart to apply..."
+		$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+		exit
 
-	  }
-	} } else { Write-Host "Invalid input.`n" -ForegroundColor Red } }
+		  }
+		} } else { Write-Host "Invalid input.`n" -ForegroundColor Red } }
