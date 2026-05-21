57253260e0276e393013241deb39b7b9829543e5
iFryno <thealmightyfryno@gmail.com>
2026-05-21 13:22:44 +0200

    Update Legacy File Explorer.ps1

--- Legacy File Explorer.ps1
+++ Legacy File Explorer.ps1
@@ -14,6 +14,7 @@
 	Write-Host "Restore the Legacy File Explorer on Windows 11`n"
 	Write-Host "1. Legacy"
 	Write-Host "2. Default`n"
+
 	while ($true) {
 	$choice = Read-Host " "
 	if ($choice -match '^[1-2]$') {
