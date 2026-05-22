43f36c606beab976c68fd8f9e6495a5062150a5e
iFryno <thealmightyfryno@gmail.com>
2026-05-22 20:48:16 +0200

    Update Core Parking.ps1

--- Core Parking.ps1
+++ Core Parking.ps1
@@ -1,59 +1,58 @@
-		if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
-		{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-		exit}
+	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+	{Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+	exit}
 
-		$Host.UI.RawUI.WindowTitle = "Administrator: " + (Split-Path -Leaf $myInvocation.MyCommand.Definition)
-		$Host.UI.RawUI.BackgroundColor = "Black"
-		$Host.PrivateData.ProgressBackgroundColor = "Black"
-		$Host.PrivateData.ProgressForegroundColor = "White"
-		Clear-Host
+	$Host.UI.RawUI.WindowTitle = "Administrator: " + (Split-Path -Leaf $myInvocation.MyCommand.Definition)
+	$Host.UI.RawUI.BackgroundColor = "Black"
+	$Host.PrivateData.ProgressBackgroundColor = "Black"
+	$Host.PrivateData.ProgressForegroundColor = "White"
 
-		Write-Host "Core Parking`n"
-		Write-Host "1. Disable"
-		Write-Host "2. Enable`n"
+	Write-Host "Core Parking`n"
+	Write-Host "1. Disable"
+	Write-Host "2. Enable`n"
 
-		while ($true) {
-		$choice = Read-Host " "
-		if ($choice -match '^[1-2]$') {
-		switch ($choice) {
-		1 {
+	while ($true) {
+	$choice = Read-Host " "
+	if ($choice -match '^[1-2]$') {
+	switch ($choice) {
+	1 {
 
-		Clear-Host
+	Clear-Host
 
-# Unhide processor performance core parking min cores
-powercfg /attributes SUB_PROCESSOR CPMINCORES -ATTRIB_HIDE
+	# Unhide processor performance core parking min cores
+	powercfg /attributes SUB_PROCESSOR CPMINCORES -ATTRIB_HIDE
 
-# Unpark CPU cores
-powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 100
-powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 100
+	# Unpark CPU cores
+	powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 100
+	powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 100
 
-# Apply changes
-powercfg /setactive SCHEME_CURRENT
+	# Apply changes
+	powercfg /setactive SCHEME_CURRENT
 
-# Open power plan settings
-Start-Process powercfg.cpl
+	# Open power plan settings
+	Start-Process powercfg.cpl
 
-		exit
+	exit
 
-		  }
-		2 {
+	  }
+	2 {
 
-		Clear-Host
+	Clear-Host
 
-# Unhide processor performance core parking min cores
-powercfg /attributes SUB_PROCESSOR CPMINCORES -ATTRIB_HIDE
+	# Unhide processor performance core parking min cores
+	powercfg /attributes SUB_PROCESSOR CPMINCORES -ATTRIB_HIDE
 
-# Park CPU cores
-powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 10
-powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 10
+	# Park CPU cores
+	powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 10
+	powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR CPMINCORES 10
 
-# Apply changes
-powercfg /setactive SCHEME_CURRENT
+	# Apply changes
+	powercfg /setactive SCHEME_CURRENT
 
-# Open power plan settings
-Start-Process powercfg.cpl
+	# Open power plan settings
+	Start-Process powercfg.cpl
 
-		exit
+	exit
 
-		  }
-		} } else { Write-Host "Invalid input.`n" -ForegroundColor Red } }
+	  }
+	} } else { Write-Host "Invalid input.`n" -ForegroundColor Red } }
