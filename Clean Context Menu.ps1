96d392b4f5ea2d1ee0ebceeb4058e67a1e85256e
iFryno <thealmightyfryno@gmail.com>
2026-06-07 17:48:12 +0200

    Update Clean Context Menu.ps1

--- Clean Context Menu.ps1
+++ Clean Context Menu.ps1
@@ -21,7 +21,7 @@ Write-Host "Enabling..." -NoNewline
 $regContent = @"
 Windows Registry Editor Version 5.00
 
-; Enable classic context menu
+; Remove modern context menu
 [HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32]
 @=""
 
