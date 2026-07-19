dad3679ce20ce51fe8bac1f227361052f143000d
iFryno <thealmightyfryno@gmail.com>
2026-07-19 21:24:56 +0200

    Update Legacy Settings.ps1

--- Legacy Settings.ps1
+++ Legacy Settings.ps1
@@ -23,18 +23,21 @@ Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization' /v 'Icon' /t REG_SZ /
 Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization' /v 'MUIVerb' /t REG_SZ /d 'Legacy settings' /f *>$null
 Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization' /v 'Position' /t REG_SZ /d 'Bottom' /f *>$null
 Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization' /v 'SubCommands' /t REG_SZ /d '""' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\005flyout' /v 'Icon' /t REG_SZ /d 'SndVol.exe,-101' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\005flyout' /v 'MUIVerb' /t REG_SZ /d 'Sound' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\005flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL mmsys.cpl,,0' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\006flyout' /v 'Icon' /t REG_SZ /d 'powercpl.dll,0' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\006flyout' /v 'MUIVerb' /t REG_SZ /d 'Power Options' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\006flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL powercfg.cpl' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\008flyout' /v 'Icon' /t REG_SZ /d 'main.cpl' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\008flyout' /v 'MUIVerb' /t REG_SZ /d 'Mouse Properties' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\008flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL main.cpl,,0' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\009flyout' /v 'Icon' /t REG_SZ /d 'desk.cpl' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\009flyout' /v 'MUIVerb' /t REG_SZ /d 'Desktop Icon Settings' /f *>$null
-Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\009flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL desk.cpl,,0' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\001flyout' /v 'Icon' /t REG_SZ /d 'SndVol.exe,-101' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\001flyout' /v 'MUIVerb' /t REG_SZ /d 'Sound' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\001flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL mmsys.cpl,,0' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\002flyout' /v 'Icon' /t REG_SZ /d 'powercpl.dll,0' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\002flyout' /v 'MUIVerb' /t REG_SZ /d 'Power Options' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\002flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL powercfg.cpl' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\003flyout' /v 'Icon' /t REG_SZ /d 'main.cpl' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\003flyout' /v 'MUIVerb' /t REG_SZ /d 'Mouse Properties' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\003flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL main.cpl,,0' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\004flyout' /v 'Icon' /t REG_SZ /d 'SystemPropertiesAdvanced.exe' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\004flyout' /v 'MUIVerb' /t REG_SZ /d 'System Properties' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\004flyout\command' /ve /t REG_SZ /d 'SystemPropertiesAdvanced.exe' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\005flyout' /v 'Icon' /t REG_SZ /d 'desk.cpl' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\005flyout' /v 'MUIVerb' /t REG_SZ /d 'Desktop Icon Settings' /f *>$null
+Reg.exe add 'HKCR\DesktopBackground\Shell\Personalization\shell\005flyout\command' /ve /t REG_SZ /d 'rundll32.exe shell32.dll,Control_RunDLL desk.cpl,,0' /f *>$null
 
 Clear-Host
 Write-Host "Legacy Settings added.`n" -ForegroundColor Green
