d0e8f74e38e90254efd7bf8a2b701699e695df3b
iFryno <thealmightyfryno@gmail.com>
2026-07-24 07:31:03 +0200

    Update and rename Browser Policies.ps1 to BrowserPolicies.ps1

---
+++ BrowserPolicies.ps1
@@ -0,0 +1,80 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Start-Process PowerShell.exe -ArgumentList ('-NoProfile -ExecutionPolicy Bypass -File "{0}"' -f $PSCommandPath) -Verb RunAs
+    exit
+}
+
+$Host.UI.RawUI.BackgroundColor = 'Black'
+$Host.UI.RawUI.ForegroundColor = 'White'
+Clear-Host
+
+Write-Host "Browser Policies`n"
+Write-Host '1. Edge'
+Write-Host '2. Brave'
+Write-Host '3. Chrome'
+Write-Host "4. Remove`n"
+
+while ($true) {
+    $choice = Read-Host ' '
+
+    if ($choice -notmatch '^[1-4]$') {
+        Write-Host "Invalid option.`n" -ForegroundColor Red
+        continue
+    }
+
+    switch ($choice) {
+
+        1 {
+            # Add policies to Edge
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Edge' /v 'StartupBoostEnabled' /t REG_DWORD /d '0' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Edge' /v 'BackgroundModeEnabled' /t REG_DWORD /d '0' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Edge' /v 'HardwareAccelerationModeEnabled' /t REG_DWORD /d '0' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Edge' /v 'GenAILocalFoundationalModelSettings' /t REG_DWORD /d '1' /f *>$null
+
+            Clear-Host
+            Write-Host "Restart Edge to apply.`n" -ForegroundColor Yellow
+            Write-Host 'Press any key to exit...' -NoNewline
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            exit
+        }
+
+        2 {
+            # Add policies to Brave
+            Reg.exe add 'HKLM\SOFTWARE\Policies\BraveSoftware\Brave' /v 'BackgroundModeEnabled' /t REG_DWORD /d '0' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\BraveSoftware\Brave' /v 'HighEfficiencyModeEnabled' /t REG_DWORD /d '1' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\BraveSoftware\Brave' /v 'HardwareAccelerationModeEnabled' /t REG_DWORD /d '0' /f *>$null
+
+            Clear-Host
+            Write-Host "Restart Brave to apply.`n" -ForegroundColor Yellow
+            Write-Host 'Press any key to exit...' -NoNewline
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            exit
+        }
+
+        3 {
+            # Add policies to Chrome
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Google\Chrome' /v 'BackgroundModeEnabled' /t REG_DWORD /d '0' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Google\Chrome' /v 'HighEfficiencyModeEnabled' /t REG_DWORD /d '1' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Google\Chrome' /v 'HardwareAccelerationModeEnabled' /t REG_DWORD /d '0' /f *>$null
+            Reg.exe add 'HKLM\SOFTWARE\Policies\Google\Chrome' /v 'GenAILocalFoundationalModelSettings' /t REG_DWORD /d '1' /f *>$null
+
+            Clear-Host
+            Write-Host "Restart Chrome to apply.`n" -ForegroundColor Yellow
+            Write-Host 'Press any key to exit...' -NoNewline
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            exit
+        }
+
+        4 {
+            # Remove policies from Edge, Brave and Chrome
+            Reg.exe delete 'HKLM\SOFTWARE\Policies\Google\Chrome' /f *>$null
+            Reg.exe delete 'HKLM\SOFTWARE\Policies\Microsoft\Edge' /f *>$null
+            Reg.exe delete 'HKLM\SOFTWARE\Policies\BraveSoftware\Brave' /f *>$null
+
+            Clear-Host
+            Write-Host "Restart your browser to apply.`n" -ForegroundColor Yellow
+            Write-Host 'Press any key to exit...' -NoNewline
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            exit
+        }
+    }
+}
