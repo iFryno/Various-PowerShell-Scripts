87734de6c7a58ec6053f31457e83a798fbc6343f
iFryno <thealmightyfryno@gmail.com>
2026-07-24 07:33:03 +0200

    Update and rename DLSS Overlay.ps1 to DlssOverlay.ps1

---
+++ DlssOverlay.ps1
@@ -0,0 +1,46 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Start-Process PowerShell.exe -ArgumentList ('-NoProfile -ExecutionPolicy Bypass -File "{0}"' -f $PSCommandPath) -Verb RunAs
+    exit
+}
+
+$Host.UI.RawUI.BackgroundColor = 'Black'
+$Host.UI.RawUI.ForegroundColor = 'White'
+Clear-Host
+
+Write-Host "DLSS Overlay`n"
+Write-Host '1. Enable'
+Write-Host "2. Disable`n"
+
+while ($true) {
+    $choice = Read-Host ' '
+
+    if ($choice -notmatch '^[1-2]$') {
+        Write-Host "Invalid option.`n" -ForegroundColor Red
+        continue
+    }
+
+    switch ($choice) {
+
+        1 {
+            # Enable DLSS overlay
+            Reg.exe add 'HKLM\SOFTWARE\NVIDIA Corporation\Global\NGXCore' /v 'ShowDlssIndicator' /t REG_DWORD /d '1024' /f *>$null
+
+            Clear-Host
+            Write-Host "Changes will take effect on next game launch.`n" -ForegroundColor Yellow
+            Write-Host 'Press any key to exit...' -NoNewline
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            exit
+        }
+
+        2 {
+            # Disable DLSS overlay
+            Reg.exe delete 'HKLM\SOFTWARE\NVIDIA Corporation\Global\NGXCore' /v 'ShowDlssIndicator' /f *>$null
+
+            Clear-Host
+            Write-Host "Changes will take effect on next game launch.`n" -ForegroundColor Yellow
+            Write-Host 'Press any key to exit...' -NoNewline
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            exit
+        }
+    }
+}
